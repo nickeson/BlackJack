7bc1a5cd437dab6b571918aebe5b98fae52e15bf
Kris Nickeson <kris@siliconmtn.com>
2016-05-17 10:53:37 -0600

    initial commit

---
+++ src/com/nickeson/game_of_21/TEST.java
@@ -0,0 +1,29 @@
+package com.nickeson.game_of_21;
+
+//JDK 1.8.0
+
+/****************************************************************************
+ * <b>Title</b>: TEST.java <p/>
+ * <b>Project</b>: WebCrescendo <p/>
+ * <b>Description: </b> Put Something Here
+ * <p/>
+ * <b>Copyright:</b> Copyright (c) 2016<p/>
+ * <b>Company:</b> Silicon Mountain Technologies<p/>
+ * @author nickeson
+ * @version 1.0
+ * @since May 17, 2016<p/>
+ * updates:
+ ****************************************************************************/
+
+public class TEST {
+
+	public TEST() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
